f070195e625448c39addedc4eedd1e10241cc512
rafeekshaik <rafeekshaik365@gmail.com>
2025-03-08 15:26:40 +0530

    k

--- 02.variable.sh
+++ 02.variable.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-echo "Rahim: how are you ram"
-echo "ram: iam fine rahim"
-echo "Rahim: you are loks are too good ram"
-echo "ram: thank ypu. you are also rahim"
+NAME1=RAHIM
+NAME2=RAM
+echo "NAME1: how are you ra NAME2"
+echo "NAME2: iam fine NAME1"
+echo "NAME1: you are loks are too good NAME2"
+echo "NAME2: thank ypu. you are also NAME1"
