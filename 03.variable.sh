f4984970e82c2ce8dfaa5527fb67832cc819b193
rafeekshaik <rafeekshaik365@gmail.com>
2025-03-08 16:11:23 +0530

    jjj

---
+++ 03.variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: how are you ra $PERSON2"
+echo "$PERSON2: iam fine $PERSON1"
+echo "$PERSON1: you are loks are too good $PERSON2"
+echo "$PERSON2: thank ypu. you are also $PERSON1"
