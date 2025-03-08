81fa311ac5a31d657c412b1c51ab2bfa451d6605
rafeekshaik <rafeekshaik365@gmail.com>
2025-03-08 15:52:31 +0530

    ggg

--- 02.variable.sh
+++ 02.variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-NAME1=RAHIM
-NAME2=RAM
+NAME1=venkatesh
+NAME2=dinesh
 echo "$NAME1: how are you ra $NAME2"
 echo "$NAME2: iam fine $NAME1"
 echo "$NAME1: you are loks are too good $NAME2"
