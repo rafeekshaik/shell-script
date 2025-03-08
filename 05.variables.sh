b134275223c075c77bf2f6598e8323f4ed44d9c1
rafeekshaik <rafeekshaik365@gmail.com>
2025-03-08 16:46:48 +0530

    jj

--- 05.variables.sh
+++ 05.variables.sh
@@ -4,7 +4,8 @@
 
 echo "please enter user name::"
 read -s USERNAME
-echo "username you have entered is : $USERNAME
-echo "please enter you password"
+echo "username you have entered is :"
+echo "please enter password:$USERNAME"
 read -s PASSWORD
-echo "password you have entered is : $PASSWORD"
+echo "password you have enterd is : $PASSWORD
+
