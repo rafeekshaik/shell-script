c375c9fb2692e58383e45e94608a568070a024c5
rafeekshaik <rafeekshaik365@gmail.com>
2025-04-13 01:44:17 +0530

    lll

--- password.sh
+++ password.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "please enter user name: "
-read -S USERNAME
-echo "user name entered is :$USERNAME
+echo "please enter user name:"
+read -s USERNAME
+echo "user name entered is :$USERNAME"
