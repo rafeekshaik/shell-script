30ea7b606c326caa222289fff29e6a906928a947
rafeekshaik <rafeekshaik365@gmail.com>
2025-05-02 17:50:28 +0530

    lll

--- var.sh
+++ var.sh
@@ -2,13 +2,6 @@
 #!/bin/bash
 
 
-PERSON1=$1
-PERSON2=$2
-
-
-
-
-echo "$PERSON1:: hii $PERSON2"
-echo "$PERSON2:: hii $PERSON1"
-echo "$PERSON1:: iam fine $PERSON2 what about you "
-echo "$PERSON2:: iam also fine $PERSON1"
+echo "please enter username::"
+read USERNAME
+echo "username you have entered is :: $USERNAME"
