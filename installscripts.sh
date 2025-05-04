91fa3eda1070bc31c44841d59c3dd4dabbc10c3c
rafeekshaik <rafeekshaik365@gmail.com>
2025-05-04 13:31:09 +0530

    kkk

--- installscripts.sh
+++ installscripts.sh
@@ -4,7 +4,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
 echo "user must have previllaged access to execute the script"
+exit 1  #other then zero
 fi
 dnf install mysqll -y
 
 dnf install git -y
+ 
