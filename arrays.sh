890826d893cb208f50ca029f2fd9a32db61de226
rafeekshaik <rafeekshaik365@gmail.com>
2025-05-04 10:56:39 +0530

    kkk

--- arrays.sh
+++ arrays.sh
@@ -5,3 +5,4 @@ MOVIES=("pushpa" "rrr" "baahubali")
 echo "first movie is:: ${MOVIES[0]}"
 echo "second movie is:: ${MOVIES[1]}"
 echo "third movie is :: ${MOVIES[2]}"
+echo "all movies:: ${MOVIES[@]}"
