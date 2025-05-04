e734bc4dae1a93aa9b283b5d46086ad7e14856bc
rafeekshaik <rafeekshaik365@gmail.com>
2025-05-04 13:42:06 +0530

    lll

--- installscripts.sh
+++ installscripts.sh
@@ -8,5 +8,21 @@ exit 1  #other then zero
 fi
 dnf install mysqll -y
 
+if [ $? -ne 0 ]
+
+echo "installing mysql .... failure"
+exit 1
+else
+echo "installing mysql success"
+fi
+
+
 dnf install git -y
- 
+
+if [ $? -ne 0 ]
+then 
+echo "installing git ..... failure"
+exit 1
+else
+echo "installing git success"
+fi
