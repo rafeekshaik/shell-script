17ab7069f2730801864ef15e4806f07265ac638f
rafeekshaik <rafeekshaik365@gmail.com>
2025-05-07 21:48:18 +0530

    lll

--- practice8.sh
+++ practice8.sh
@@ -22,17 +22,17 @@ else
 echo "mysql is allready installed"
 fi
 
-# dnf list installed git
-# if [ $? -ne 0 ]
-#  then
-# dnf install git -y
-# if [ $? -ne 0 ]
-# then 
-# echo "installing git ........ failure soooo"
-# exit 1
-# else
-# echo "installing git ....... successfully"
-# fi
-# else 
-# echo "git allready installed"
+dnf list installed git
+if [ $? -ne 0 ]
+ then
+dnf install git -y
+if [ $? -ne 0 ]
+then 
+echo "installing git ........ failure soooo"
+exit 1
+else
+echo "installing git ....... successfully"
+fi
+else 
+echo "git allready installed"
 
