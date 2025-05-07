7b64c4c3f43b2b9597c3d6ae17f22ed4161e5083
rafeekshaik <rafeekshaik365@gmail.com>
2025-05-07 22:26:51 +0530

    ll

--- practice8.sh
+++ practice8.sh
@@ -2,22 +2,28 @@
 
 USERID=$(id -u)
 
+VALIDATE(){
+
+    if [ $1 -ne 0 ] 
+then
+echo "$2 ...... failure"
+exit 1
+else 
+echo "$2...... successfully"
+fi
+}
+
 if [ $USERID -ne 0 ]
 then
 echo "Error::user must have sudo access"
 exit 1
 fi
 dnf list installed mysql
+
 if [ $? -ne 0 ]
 then
 dnf install mysql -y
-if [ $? -ne 0 ] 
-then
-echo "installing mysql ...... failure"
-exit 1
-else 
-echo "installing mysql...... successfully"
-fi
+VALIDATE $? "installing mysql" 
 else
 echo "mysql is allready installed"
 fi
@@ -26,13 +32,7 @@ dnf list installed git
 if [ $? -ne 0 ]
  then
 dnf install git -y
-if [ $? -ne 0 ]
-then 
-echo "installing git ........ failure soooo"
-exit 1
-else
-echo "installing git ....... successfully"
-fi
+VALIDATE $? "installing git"
 else 
 echo "git allready installed"
 fi
