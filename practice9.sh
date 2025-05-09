9f0da54ac9b0e2ac5dd0e4f8f6c3cd861722f6a6
rafeekshaik <rafeekshaik365@gmail.com>
2025-05-09 10:10:18 +0530

    llll

---
+++ practice9.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+echo "ERROR:: user must have previllege admin access"
+exit 1
+fi
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+dnf install mysql -y
+
+
+if [ $? -ne 0 ]
+then
+echo "installing MYSQL........ failure"
+exit 1
+fi
+else 
+echo "installing MYSQL.......success"
+fi
+else 
+echo "MYSQL is allready installed"
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+echo "installing git ...... failure"
+exit 1
+fi
+else
+echo "installing git ....... success"
+fi
+else
+echo "git allready installed"
+fi
+
+
+
