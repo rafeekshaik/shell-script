80fd7d311e1a0e185843b4c86f713f5a4817fec0
rafeekshaik <rafeekshaik365@gmail.com>
2025-05-09 13:49:01 +0530

    ll

---
+++ 13-logs.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+then
+echo e "$2........ $R failure"
+exit 1
+
+else 
+echo -e "$2....... $R success"
+fi
+
+}
+
+echo "script executed at ::$TIMESTAMP &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then 
+echo "ERROR:: user must have previllege admin access"
+exit 1
+fi
+dnf list installed mysql &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then 
+dnf install mysql -y &>>$LOG_FILE_NAME
+
+
+VALIDATE $? "installing mysql"
+
+else 
+echo -e "MYSQL is allready  $Y installed"
+fi
+
+dnf list installed git &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then
+dnf install git -y &>>$LOG_FILE_NAME
+
+VALIDATE $? "installing git"
+else
+echo -e "git allready $Y installed"
+fi
+
+
+
+
