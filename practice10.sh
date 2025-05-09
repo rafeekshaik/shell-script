30e62523a74cfedce68159f7573460036e3d9562
rafeekshaik <rafeekshaik365@gmail.com>
2025-05-09 22:12:32 +0530

    lll

--- practice10.sh
+++ practice10.sh
@@ -6,32 +6,31 @@ echo "ERROR::user must have previlleged admin access"
 exit 1
 fi
 
-dnf list installed mysql
-if [ $? -ne 0 ]
-then 
-dnf install mysql -y
-if [ $? -ne 0 ]
+VALIDATE(){
+
+    if [ $? -ne 0 ]
 then 
 echo "installing mysql ...... failure"
 exit 1
 else
 echo "installing mysql....... success"
 fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+dnf install mysql -y
+VALIDATE $? "installing mysql"
 else
 echo "mysql allready installed"
-
+fi
 
   dnf list available git
   if [ $? -ne 0 ]
   then 
   dnf install git -y
-  if [ $? -ne 0 ]
-  then 
-  echo "installing git ..... failure"
-  exit 1
-  else
-  echo "installing git...... success"
-  fi
+ VALIDATE $? "installing git"
   else
   echo "git allready installed"
-  fi
+  
