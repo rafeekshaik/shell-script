8c1fe68b3195d68e9f0bb9da2b7a1824e800e17f
rafeekshaik <rafeekshaik365@gmail.com>
2025-05-09 23:44:42 +0530

    ll

--- loops.sh
+++ loops.sh
@@ -1,6 +1,39 @@
-
 #!/bin/bash
-for i in {0..10}
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOG_FOLDER="/var/log/shellscript-log"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+then 
+echo -e "$2 ......$R failure"
+exit 1
+else
+echo -e "$2......$G success"
+fi
+}
+
+
+if [ $USERID -ne 0 ]
+then 
+echo "ERROR::user must have previlleged admin access"
+exit 1
+fi
+
+
+for package in $@
 do
-echo $i
-done
+dnf list installed $package
+if [ $? -ne 0 ]
+dnf install $package -y
+VALIDATE $? "installing $package"
+else
+echo "$package allready installed"
